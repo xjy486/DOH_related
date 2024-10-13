e78afc3673934e0753104860845fcab75468b02d
qiu mi <73565979+xjy486@users.noreply.github.com>
2024-10-13 10:01:59 +0800

    Update Malicious_dnscat2_merge.py

--- dataset_merge/Malicious_dnscat2_merge.py
+++ dataset_merge/Malicious_dnscat2_merge.py
@@ -1,10 +1,20 @@
 import os
 directory='/home/ecs-user/data/PCAPs/DoHMalicious/dnscat2'
+# 获取类型
+def get_type_list():
+    types1=['default','txt']
+    types2=['tunnel','baseline']
+    
+    types=[]
+    for t1 in types1:
+        for t2 in types2:
+                t=t1+'-'+t2
+                types.append('dsncat2_'+t)
+    return types
 
-def get_filenames():
-    files_and_dirs = os.listdir(directory)
-    # ! NEED CHANGE, parameter: dsncat2_default-tunnel, dsncat2_default-baseline, dsncat2_txt-tunnel, dsncat2_txt-baseline
-    filenames = [file for file in files_and_dirs if os.path.isfile(os.path.join(directory, file)) and file.endswith(".pcap") and file.startswith('dsncat2_txt-baseline')] 
+def get_filenames(prefix):
+    files_and_dirs = os.listdir(directory) 
+    filenames = [file for file in files_and_dirs if os.path.isfile(os.path.join(directory, file)) and file.endswith(".pcap") and file.startswith(prefix)] 
     
     filenames_cloudflare={}
     filenames_google={}
@@ -16,19 +26,19 @@ def get_filenames():
         dns_service=file_split[2]
         server=file_split[3]
         if dns_service=="1111": #cloudflare
-            if server not in filenames:
+            if server not in filenames_cloudflare:
                 filenames_cloudflare[server]=[] #filenames_cloudflare['doh1']=["dns2tcp_tunnel_1111_doh1_2020-03-31T21:54:32.055088.pcap"]
             filenames_cloudflare[server].append(file)
         elif dns_service=="99911":
-            if server not in filenames:
+            if server not in filenames_quad9:
                 filenames_quad9[server]=[] #filenames_cloudflare['doh1']=["dns2tcp_tunnel_1111_doh1_2020-03-31T21:54:32.055088.pcap"]
             filenames_quad9[server].append(file)
         elif dns_service=="dnsadguardcom":
-            if server not in filenames:
+            if server not in filenames_adguard:
                 filenames_adguard[server]=[] #filenames_cloudflare['doh1']=["dns2tcp_tunnel_1111_doh1_2020-03-31T21:54:32.055088.pcap"]
             filenames_adguard[server].append(file)
         elif dns_service=="dnsgoogle":
-            if server not in filenames:
+            if server not in filenames_google:
                 filenames_google[server]=[] #filenames_cloudflare['doh1']=["dns2tcp_tunnel_1111_doh1_2020-03-31T21:54:32.055088.pcap"]
             filenames_google[server].append(file)
 
@@ -36,32 +46,36 @@ def get_filenames():
     return filenames_cloudflare,filenames_google,filenames_quad9,filenames_adguard
 
     
-def emrge(filenames_service,dns_service_name):
+def emrge(filenames_service,dns_service_name,type):
     
     # server: doh1,doh2,... filenames: a list, ["dns2tcp_tunnel_1111_doh1_2020-03-31T21:54:32.055088.pcap",...]
     for server,filename_list in filenames_service.items():
-        # ! NEED CHANGE, parameter:default-tunnel, default-baseline, txt-tunnel, txt-baseline
-        output=f"./dnscat2/txt-baseline/{dns_service_name}/dnscat2_txt-baseline_{server}.pcap"
+        folder=type.split('_')[1] # default-tunnel ...
+        output=f"./dnscat2/{folder}/{dns_service_name}/{type}_{server}.pcap"
         # make filename sorted by time
         filename_list=sorted(filename_list)
         filename_concat=''
         for filename in filename_list:
             filename_concat=filename_concat+filename+' '
         cmd=f'mergecap -a {filename_concat} -w {output}'
-        try:
-            os.system(cmd)
-            print(f"{dns_service_name} success!")
-        except Exception as e:
-            print(f"{dns_service_name} falied: {e}")
-    
+        os.system(cmd)
+    # print(f"{dns_service_name} {type}_{server} success!")
+
 
 def main():
-    filenames_cloudflare,filenames_google,filenames_quad9,filenames_adguard=get_filenames()
-    emrge(filenames_cloudflare,'Cloudflare')
-    emrge(filenames_adguard,"AdGuard")
-    emrge(filenames_google,"Google")
-    emrge(filenames_quad9,'Quad9')
-
+    types=get_type_list()
+    for type in types:
+        filenames_cloudflare,filenames_google,filenames_quad9,filenames_adguard=get_filenames(type)
+        print(f"{type}")
+        emrge(filenames_cloudflare,'Cloudflare',type)
+        
+        emrge(filenames_adguard,"AdGuard",type)
+        
+        emrge(filenames_google,"Google",type)
+        
+        emrge(filenames_quad9,'Quad9',type)
+        
 
 if __name__ == '__main__':
     main()
+
